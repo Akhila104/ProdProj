12beb9c2956fedf2bd7028bde692aef7bab2a07c
Akhila104 <96979129+Akhila104@users.noreply.github.com>
2022-02-03 21:34:08 +0530

    Update CrudApplication.java

--- src/main/java/com/example/Crud/CrudApplication.java
+++ src/main/java/com/example/Crud/CrudApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudApplication {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
+	try {
 		SpringApplication.run(CrudApplication.class, args);
+	} catch (Exception e) {
+		e.printStackTrace();
 	}
+}
 
 }
